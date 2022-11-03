6abfc18dc2ae52392618a97d9feb99e582ee3431
klebiedzinski <lebiedzinskik02@gmail.com>
2022-11-03 15:10:39 +0100

    lab04

---
+++ lab04/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.json())
+
+app.listen( 
+    PORT,    
+    () =>{
+        console.log('port działa: ' + PORT)
+    }
+)
+
+app.get('/get', (req, res) => {
+    res.status(200).send("get request chodzi")
+});
+
+app.post('post', (req, res) => {
+    const {dane} = req.body;
+
+    res.send("dodano dane na serwer:"+dane)
+})
