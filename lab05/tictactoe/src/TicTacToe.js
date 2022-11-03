3142717a6f9c9e768cc80b1e4624616c4609283c
Karol Lebiedziński <lebiedzinskik02@gmail.com>
2022-11-03 17:28:39 +0100

    ttt

--- lab05/tictactoe/src/TicTacToe.js
+++ lab05/tictactoe/src/TicTacToe.js
@@ -1,28 +1,25 @@
 
 function TicTacToe() {
   return (
-    <div className="container">
-        <section class="title">
-            <h1>Tic Tac Toe</h1>
-        </section>
-        <section class="display">
-            Player <span class="display-player playerX">X</span>'s turn
-        </section>
-        <section class="container">
-            <div class="tile"></div>
-            <div class="tile"></div>
-            <div class="tile"></div>
-            <div class="tile"></div>
-            <div class="tile"></div>
-            <div class="tile"></div>
-            <div class="tile"></div>
-            <div class="tile"></div>
-            <div class="tile"></div>
-        </section>
-        <section class="display announcer hide"></section>
-        <section class="controls">
+    <div className="TicTacToe">
+        <h1>Tic Tac Toe</h1>
+        <div className="info">
+            Your turn!
+        </div>
+        <div className="container">
+            <div className="field"></div>
+            <div className="field"></div>
+            <div className="field"></div>
+            <div className="field"></div>
+            <div className="field"></div>
+            <div className="field"></div>
+            <div className="field"></div>
+            <div className="field"></div>
+            <div className="field"></div>
+       
             <button id="reset">Reset</button>
-        </section>
+            
+        </div>
     </div>
   );
 }
